c3fa32ebee31f0f0330d4020750a06f6f9aeaf65
luca@mint <luca.rasconi@gmail.com>
2017-09-28 10:49:52 +0200

    Collector

--- LoadStatistics.js
+++ LoadStatistics.js
@@ -1,9 +1,13 @@
-const Page = require('./Page');
-const Video = require('./Video');
-const Post = require('./Post');
-const post = require('./post');
-const Statistic = require('./Statistic');
-const access_token = require('./Token').access_token;
+const Page = require("./Page");
+const Video = require("./Video");
+const Post = require("./Post");
+const Collector = require("./Collector");
+const Statistic = require("./Statistic");
+const access_token = require("./Token").access_token;
 
-Video.getAll().then(post.collectStatistics(access_token)).then(Statistic.vsave);
-Post.getAll().then(post.collectStatistics(access_token)).then(Statistic.psave);
+Video.getAll()
+  .then(Collector.collectStatistics(access_token))
+  .then(Statistic.vsave);
+Post.getAll()
+  .then(Collector.collectStatistics(access_token))
+  .then(Statistic.psave);
