c21a16f219a5a1001fb782f87b32e16197a80ebf
luca rasconi <luca.rasconi@smallfish.com>
2017-10-11 14:02:33 +0000

    post to PostStatsCollector

--- LoadStatistics.js
+++ LoadStatistics.js
@@ -1,9 +1,9 @@
 const Page = require('./Page');
 const Video = require('./Video');
 const Post = require('./Post');
-const post = require('./post');
+const PostStatsCollector = require('./PostStatsCollector');
 const Statistic = require('./Statistic');
 const access_token = require('./Token').access_token;
 
-Video.getAll().then(post.collectStatistics(access_token)).then(Statistic.vsave);
-Post.getAll().then(post.collectStatistics(access_token)).then(Statistic.psave);
+Video.getAll().then(PostStatsCollector.collectStatistics(access_token)).then(Statistic.vsave);
+Post.getAll().then(PostStatsCollector.collectStatistics(access_token)).then(Statistic.psave);
