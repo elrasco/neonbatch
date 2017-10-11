6a90c75a7f188aac2d281ec95e8a7945fa9352ae
luca rasconi <luca.rasconi@smallfish.com>
2017-10-11 13:55:31 +0000

    User.js edited online with Bitbucket

--- User.js
+++ User.js
@@ -2,5 +2,5 @@ const R = require('ramda');
 const fbApi = require('./fbApi');
 
 module.exports = {
-  getPagesILike: access_token => fbApi.get('/me/likes?limit=50', { access_token }).then(result => result.data)
+  getPagesILike: access_token => fbApi.get('/me/likes?limit=70&fields=name,id,picture,created_time', { access_token }).then(result => result.data)
 }
