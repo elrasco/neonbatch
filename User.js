ba7b209ed79ef34bb90d396ac1a1881767a2b2e2
luca rasconi <luca.rasconi@smallfish.com>
2017-10-11 16:57:33 +0200

    page picture

--- User.js
+++ User.js
@@ -2,5 +2,14 @@ const R = require('ramda');
 const fbApi = require('./fbApi');
 
 module.exports = {
-  getPagesILike: access_token => fbApi.get('/me/likes?limit=70&fields=name,id,picture,created_time', { access_token }).then(result => result.data)
+  getPagesILike: access_token => fbApi.get('/me/likes?limit=70&fields=name,id,picture,created_time', { access_token })
+    .then(result => result.data)
+    .then(pages => pages.map(page => {
+      return {
+        name: page.name,
+        id: page.id,
+        picture: page.picture.data.url,
+        created_time: page.created_time
+      }
+    }))
 }
